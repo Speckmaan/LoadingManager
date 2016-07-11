022aa0137fe1f9a557ce5806b865a0bb4de79338
Lauritz Klaus <lauritz@r-w-klaus.de>
2016-07-11 19:59:11 +0200

    Version 0.03
    +Vektor vDataList zum Verfolgen von geöffneten Dateien

--- LoadingManager/LoadingManager.cpp
+++ LoadingManager/LoadingManager.cpp
@@ -9,6 +9,7 @@ int main()
 {
 	string sEingabe = "1";
 	int Eingabe = 1;
+	vector<vector <string>> vDataList;				//1 für out, 2 für in
 
 	while (Eingabe != 0)
 	{
@@ -29,6 +30,8 @@ int main()
 			fstream exData;
 			string sDataName;
 			string sNewData;
+			vector <string> vNewFile;
+			vector<vector<string>>::iterator vToDelete;
 
 			cout << "Name der Datei: " << endl;
 			getline(cin, sDataName);
@@ -37,12 +40,68 @@ int main()
 
 			// Überprüfen ob die Datei schon existiert
 			exData.open(sDataName, ios::in);
+			cout << "\n" <<"Momentan geöffnete Dateien: \n";
+			vNewFile.push_back(sDataName);
+			vNewFile.push_back("in");
+
+			vDataList.push_back(vNewFile);
+			vNewFile.erase(vNewFile.begin(), vNewFile.end());
+
+			for (vector<vector<string>>::iterator vi = vDataList.begin(); vi != vDataList.end(); vi++)
+			{
+				for (vector<string>::iterator vj = vi->begin(); vj != vi->end(); vj++)
+				{
+					if (vj == vi->end() - 1)
+					{
+						cout << *vj << "\n";
+					}
+					else
+					{
+						cout << *vj << " ";
+					}
+				}
+			}
+			
+			cout << endl;
+
 			if (!exData)
 			{
 				cout << "Die Datei existiert nochnicht! Soll eine neue Datei erstellt werden? [Y/N]\n";
 				getline(cin, sNewData);
 
 				exData.close();
+
+				cout << "\n" << "Momentan geöffnete Dateien: \n";
+				for (vector<vector<string>>::iterator vi = vDataList.begin(); vi != vDataList.end(); vi++)
+				{
+					vector<string>::iterator vj = find(vi->begin(), vi->end(), sDataName);
+					if (vj != vi->end())
+					{
+						vToDelete = vi;
+					}
+					else
+					{
+						cout << sDataName << " konnte nicht aus der Liste entfernt werden! \n";
+					}
+				}
+				vDataList.erase(vToDelete);
+
+				for (vector<vector<string>>::iterator vi = vDataList.begin(); vi != vDataList.end(); vi++)
+				{
+					for (vector<string>::iterator vj = vi->begin(); vj != vi->end(); vj++)
+					{
+						if (vj == vi->end() - 1)
+						{
+							cout << *vj << "\n";
+						}
+						else
+						{
+							cout << *vj << " ";
+						}
+					}
+				}
+
+				cout << endl;
 
 				if (sNewData == "Y" || sNewData == "y")
 				{
